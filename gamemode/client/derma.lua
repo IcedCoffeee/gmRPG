a8ea5bca1d113db64085c0d617faa15259f8a60a
AlmightyLaxz <almightylaxz@gmail.com>
2016-02-27 22:30:26 +1030

    Added a Character Menu

--- gamemode/client/derma.lua
+++ gamemode/client/derma.lua
@@ -409,3 +409,63 @@ net.Receive( "rpgCivilianDermaStart", function()
 		frame:Close()
 	end
 end)
+
+local charUIVisible = false
+
+hook.Add("Think", "rpg_CharacterMenu", function()
+	if input.IsKeyDown(KEY_C) && !gui.IsGameUIVisible() && !charUIVisible then
+
+		local frame = vgui.Create( "DFrame" )
+		frame:SetPos( 5, 5 )
+		frame:SetSize( 500, 600 )
+		frame:SetTitle("Character Menu")
+		frame:SetVisible( true )
+		frame:SetDraggable( true )
+		frame:ShowCloseButton( false )
+		frame.Paint = function( self, w, h )
+			draw.RoundedBox( 0, 0, 0, w, h, Color( 0, 0, 0, 200 ) )
+		end
+		frame:MakePopup()
+		frame:Center()
+	
+		local DLabel = vgui.Create( "DLabel", frame )
+		DLabel:SetPos( 10, 140 )
+		DLabel:SetText(LocalPlayer():Nick())
+		DLabel:SetSize(300, 80)
+		DLabel:SetFont("hud_font")
+		DLabel:SetTextColor(Color(255, 255, 255))
+
+		local DLabel = vgui.Create( "DLabel", frame )
+		DLabel:SetPos( 10, 200 )
+		DLabel:SetText("Strength: " .. ply:GetNWString("gmrpg_strength"))
+		DLabel:SetSize(300, 80)
+		DLabel:SetFont("hud_font")
+		DLabel:SetTextColor(Color(255, 255, 255))
+
+		local DLabel = vgui.Create( "DLabel", frame )
+		DLabel:SetPos( 10, 260 )
+		DLabel:SetText("Intelligence: " .. ply:GetNWString("gmrpg_intelligence"))
+		DLabel:SetSize(300, 80)
+		DLabel:SetFont("hud_font")
+		DLabel:SetTextColor(Color(255, 255, 255))
+
+		local icon = vgui.Create( "DModelPanel", frame )
+		icon:SetSize( 300, 600 )
+		icon:SetPos(250, 20)
+		icon:SetModel(LocalPlayer():GetModel())
+		icon:SetFOV(20)
+		local headpos = icon.Entity:GetBonePosition( icon.Entity:LookupBone( "ValveBiped.Bip01_Head1" ) )
+		icon:SetCamPos( headpos-Vector( -200, 0, 0 ) )
+		function icon:LayoutEntity( Entity ) return end
+
+		local closeButton = vgui.Create( "DButton", frame )
+		closeButton:SetPos( 200, 550 )
+		closeButton:SetText( "Close" )
+		closeButton:SetSize( 100, 30 )
+		closeButton.DoClick = function()
+			charUIVisible = false
+			frame:Close()
+		end
+		charUIVisible = true
+	end
+end)
