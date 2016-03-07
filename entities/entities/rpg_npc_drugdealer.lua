7fead13b181740b75a47e2e7a7baa88065cbc294
AlmightyLaxz <almightylaxz@gmail.com>
2016-03-07 20:06:51 +1030

    new npc and item

---
+++ entities/entities/rpg_npc_drugdealer.lua
@@ -0,0 +1,42 @@
+AddCSLuaFile()
+
+ENT.Base            = "rpg_npc_merchant_base"
+ENT.Type            = "ai"
+
+ENT.PrintName       = "NPC Drug Dealer"
+ENT.Author          = "Almighty Laxz"
+ENT.Contact         = ""
+ENT.Purpose         = ""
+ENT.Instructions    = ""
+ENT.Category        = "gmRPG"
+
+ENT.Spawnable       = true
+
+local merchantText = "What do you want?"
+
+if SERVER then
+    function ENT:Initialize( )
+    	self:SetModel( "models/player/group03/male_03.mdl" )
+    	self:SetHullType( HULL_HUMAN )
+    	self:SetHullSizeNormal( )
+    	self:SetNPCState( NPC_STATE_SCRIPT )
+    	self:SetSolid(  SOLID_BBOX )
+    	self:CapabilitiesAdd( CAP_ANIMATEDFACE, CAP_TURN_HEAD )
+    	self:SetUseType( SIMPLE_USE )
+    	self:DropToFloor()
+    end
+
+    function ENT:AcceptInput( Name, Activator, Caller )
+
+    	if !Activator.cantUse and Activator:IsPlayer() then
+    		Activator.cantUse = true
+    		net.Start( "rpgDrugDermaStart" )
+                net.WriteString(merchantText)
+                net.WriteEntity(self)
+    		net.Send( Activator )
+    		timer.Simple(1, function()
+    			Activator.cantUse = false
+    		end)
+    	end
+    end
+end
--- gamemode/client/merchants.lua
+++ gamemode/client/merchants.lua
@@ -4,9 +4,11 @@ Coffee = { "models/props_junk/garbage_coffeemug001a.mdl", "$10", "chat.AddText(\
 StrongCoffee = { "models/props_junk/garbage_coffeemug001a.mdl", "$15", "chat.AddText(\"You ask for a Strong Coffee\")", "Strong Coffee"}
 EnergyDrink = { "models/props_junk/PopCan01a.mdl", "$5", "chat.AddText(\"You ask for an Energy Drink\")", "Energy Drink"}
 Noodles = { "models/props_junk/garbage_takeoutcarton001a.mdl", "$5", "chat.AddText(\"You ask for Noodles\")", "Noodles"}
+CaffeinePills = { "models/props_lab/jar01b.mdl", "$30", "chat.AddText(\"You ask for Caffeine Pills\")", "Caffeine Pills"}
 
 local BarItems = {Beer, StrongBeer}
 local CafeItems = {Coffee, StrongCoffee, EnergyDrink, Noodles}
+local DrugItems = {CaffeinePills}
 
 function BarMenu()
 	local barText = net.ReadString()
@@ -140,3 +142,69 @@ function CafeMenu()
 end
 
 net.Receive( "rpgCafeDermaStart", CafeMenu)
+
+function DrugMenu()
+	local drugText = net.ReadString()
+	local npcEnt = net.ReadEntity()
+
+	local frame = vgui.Create( "DFrame" )
+	frame:SetPos( 5, 5 )
+	frame:SetSize( 500, 300 )
+	frame:SetTitle( "Cafe Worker" )
+	frame:SetVisible( true )
+	frame:SetDraggable( true )
+	frame:ShowCloseButton( false )
+	frame.Paint = function( self, w, h )
+		draw.RoundedBox( 0, 0, 0, w, h, Color( 0, 0, 0, 200 ) )
+	end
+	frame:MakePopup()
+	frame:Center()
+
+	local DLabel = vgui.Create( "DLabel", frame )
+	DLabel:SetPos( 10, 10 )
+	DLabel:SetText(drugText)
+	DLabel:SetSize(200, 80)
+
+	local invlist	= vgui.Create( "DIconLayout", frame )
+	invlist:SetSize( 340, 200 )
+	invlist:SetPos( 50, 70 )
+	invlist:SetSpaceY( 5 ) //Sets the space in between the panels on the X Axis by 5
+	invlist:SetSpaceX( 5 ) //Sets the space in between the panels on the Y Axis by 5
+
+	for k,v in pairs(DrugItems) do
+
+		local model   =   v[1]
+		local tooltip =   v[2]
+		local func    =   v[3]
+		local text    =   v[4]
+
+		local item = invlist:Add( "DModelPanel" )
+		item:SetSize( 80, 80 )
+		item:SetModel(model)
+		item:SetTooltip(tooltip)
+		item:SetLookAt(item.Entity:GetPos())
+		item:SetFOV(10)
+		item.DoClick = function()
+			RunString(func)
+			net.Start("requestPurchase")
+				net.WriteString(text)
+			net.SendToServer()
+		end
+
+		item.label = vgui.Create("DLabel", item)
+		item.label:SetText(text)
+		item.label:SizeToContents()
+		item.label:Center()
+		item.label:SetContentAlignment(5)
+	end
+
+	local closeButton = vgui.Create( "DButton", frame )
+	closeButton:SetPos( 200, 250 )
+	closeButton:SetText( "Close" )
+	closeButton:SetSize( 120, 40 )
+	closeButton.DoClick = function()
+		frame:Close()
+	end
+end
+
+net.Receive( "rpgDrugDermaStart", CafeMenu)
